34b5874593508a6562ef27d16ecfbd633f35a408
pabloipl <p.perez14@ufromail.cl>
2022-09-13 09:51:52 -0300

    Commit Pablo

--- src/main/java/calculadoraCientifica.java
+++ src/main/java/calculadoraCientifica.java
@@ -1,4 +1,19 @@
 public class calculadoraCientifica {
+    public static void main(String args[]){
+    }
 
+    public static double perimetroCuadrado(double lado){
+        return (4*lado);
+    }
+
+    public static double perimetroTriangulo(double ladoa, double ladob){
+        return (2*ladoa)+(2*ladob);
+    }
+    public static double perimetroCirculo(double r){
+        return (2*r*Math.PI);
+    }
+    public static double perimetroEsfera(){
+
+    }
 
 }
